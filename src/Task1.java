645880595ffefa353e691cb7ded7be9f61dbd237
ElenaH <info.elenna@gmail.com>
2024-10-29 19:01:18 +0100

    first commit

---
+++ src/Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+    public static void main(String[] args) {
+        double priseA = 1000.00;
+        double priseB = 500.00;
+        double summa = priseA+priseB;
+        double skidka = summa * 0.10;
+        double zena = summa-skidka;
+
+        System.out.println("Skidka: " +skidka);
+        System.out.println("Zena: " +zena);
+
+    }
+}
